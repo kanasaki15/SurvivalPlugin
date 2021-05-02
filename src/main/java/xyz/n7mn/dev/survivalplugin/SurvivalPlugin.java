7a5a818e4de81f0e692c8fdcf81bbfbfd74245dc
kanasaki15 <kassi_0123@kassi-hp.info>
2021-05-03 02:48:55 +0900

    資源ワールド定期再生成処理

--- src/main/java/xyz/n7mn/dev/survivalplugin/SurvivalPlugin.java
+++ src/main/java/xyz/n7mn/dev/survivalplugin/SurvivalPlugin.java
@@ -3,10 +3,14 @@ package xyz.n7mn.dev.survivalplugin;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 import xyz.n7mn.dev.survivalplugin.command.MainCommand;
+import xyz.n7mn.dev.survivalplugin.command.SetHomeCommand;
 import xyz.n7mn.dev.survivalplugin.command.SigenCommand;
+import xyz.n7mn.dev.survivalplugin.command.UserCommand;
 import xyz.n7mn.dev.survivalplugin.data.PlayerLocationData;
 import xyz.n7mn.dev.survivalplugin.listener.EventListener;
+import xyz.n7mn.dev.survivalplugin.timer.WorldReCreateTimer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +18,10 @@ import java.util.UUID;
 
 public final class SurvivalPlugin extends JavaPlugin {
 
+    private Boolean isMoveWorld = true;
     private List<UUID> list = new ArrayList<>();
     private List<PlayerLocationData> PlayerList = new ArrayList<>();
+    private BukkitTask task = null;
 
     @Override
     public void onEnable() {
@@ -28,10 +34,14 @@ public final class SurvivalPlugin extends JavaPlugin {
 
         getLogger().info("sigenワールドの読み込みに成功しました。 seed : " + sigen.getSeed());
 
-        getCommand("sigen").setExecutor(new SigenCommand(PlayerList));
+        getCommand("sigen").setExecutor(new SigenCommand(PlayerList, isMoveWorld));
         getCommand("main").setExecutor(new MainCommand(PlayerList));
+        getCommand("user").setExecutor(new UserCommand());
+        getCommand("sethome").setExecutor(new SetHomeCommand());
 
         getServer().getPluginManager().registerEvents(new EventListener(this, list), this);
+
+        getServer().getScheduler().runTaskTimerAsynchronously(this, new WorldReCreateTimer(this), 0L, 20L);
 
         getLogger().info(getName() + " Ver "+getDescription().getVersion()+" 起動しました。");
     }
@@ -44,6 +54,20 @@ public final class SurvivalPlugin extends JavaPlugin {
             data.getPlayer().teleport(data.getLocation());
         }
 
+        PlayerList = new ArrayList<>();
+
         getLogger().info(getName() + " Ver "+getDescription().getVersion()+" 終了しました。");
+    }
+
+    public List<PlayerLocationData> getPlayerList() {
+        return PlayerList;
+    }
+
+    public boolean isMoveWorld() {
+        return isMoveWorld;
+    }
+
+    public void setMoveWorld(boolean moveWorld) {
+        isMoveWorld = moveWorld;
     }
 }
--- src/main/java/xyz/n7mn/dev/survivalplugin/command/SigenCommand.java
+++ src/main/java/xyz/n7mn/dev/survivalplugin/command/SigenCommand.java
@@ -15,9 +15,11 @@ import java.util.List;
 public class SigenCommand implements CommandExecutor {
 
     private List<PlayerLocationData> locationDataList;
+    private Boolean isMoveWorld;
 
-    public SigenCommand(List<PlayerLocationData> locationDataList){
+    public SigenCommand(List<PlayerLocationData> locationDataList, Boolean isMoveWorld){
         this.locationDataList = locationDataList;
+        this.isMoveWorld = isMoveWorld;
     }
 
     @Override
@@ -27,11 +29,15 @@ public class SigenCommand implements CommandExecutor {
 
             Player player = (Player) sender;
 
-            locationDataList.add(new PlayerLocationData(player, player.getLocation()));
-            Location location = Bukkit.getWorld("sigen").getSpawnLocation();
-            player.teleport(location);
+            if (isMoveWorld){
+                locationDataList.add(new PlayerLocationData(player, player.getLocation()));
+                Location location = Bukkit.getWorld("sigen").getSpawnLocation();
+                player.teleport(location);
 
-            player.sendMessage(ChatColor.YELLOW + "[ななみ生活鯖] " + ChatColor.RESET + "資源ワールドへ移動しました。 戻るには「/main」と入力してください。");
+                player.sendMessage(ChatColor.YELLOW + "[ななみ生活鯖] " + ChatColor.RESET + "資源ワールドへ移動しました。 戻るには「/main」と入力してください。");
+            } else {
+                player.sendMessage(ChatColor.YELLOW + "[ななみ生活鯖] " + ChatColor.RED + "現在資源ワールドへの移動は禁止されています。");
+            }
 
         }
 
